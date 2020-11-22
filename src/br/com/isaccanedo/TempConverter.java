95f6a69ef430c1c02fd8b8c774f87bfb79bc749b
isaccanedo <isaccanedo@gmail.com>
2020-11-22 10:59:17 -0300

    :bulb: Initial commit

---
+++ src/br/com/isaccanedo/TempConverter.java
@@ -0,0 +1,33 @@
+package br.com.isaccanedo;
+
+/**
+ * @author Isac Canedo
+ */
+
+public class TempConverter {
+
+    public static void main(String[] args) {
+        TempConverter t = new TempConverter();
+        t.start();
+        t.data();
+        t.end();
+    }
+
+    protected void start() {
+    }
+
+    protected void data() {
+        for (int i=-40; i<=120; i+=10) {
+            float c = (i-32)*(5f/9);
+            print(i, c);
+        }
+    }
+
+    protected void print(float f, float c) {
+        System.out.println(f + " " + c);
+    }
+
+    protected void end() {
+    }
+
+}
